80b709bce308befc9dd999c93f8b34cd9af6dc05
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-04-10 16:13:44 -0500

    added timers and on start stuff...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
 // event listener's
+$("#remaining-time").hide();
+$("#start").on('click', trivia.startGame);
+$(document).on('click' , '.option', trivia.guessChecker);
+})
 // trivia timer/count
+var trivia ={
+  correct: 0,
+  incorrect: 0,
+  unanswered: 0,
+  currentSet: 0,
+  timer: 10,
+  timerOn: false,
+  timerID : '',
+
+//questions and answers data
+question: {
+  
+}
+
+}
+
 //questions and answers data
 //start game
 //restart game
