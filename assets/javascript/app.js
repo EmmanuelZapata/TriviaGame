9a3d1a3be918e938648872a7f58cf6e0fe95915c
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-04-10 16:07:40 -0500

    psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+// event listener's
+// trivia timer/count
+//questions and answers data
+//start game
+//restart game
+//next question loop
+//set timer
+//index questions
+//array of questions
+//push to html
+//timer out
+//all questions shown/answered
+//get result
+//else if statement
+//remove statements
+//reset game
